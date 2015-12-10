2724ed8da81ed0b5abcabb73ccf6b476b6c3ee3b
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-12-10 15:37:57 -0800

    updated test and build files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <cpr.h>
 
+#include <stdio.h>
+#include <iostream>
+
 int main(int argc, char** argv) {
-    auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-                      cpr::Authentication{"user", "pass"},
-                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-    r.status_code;                  // 200
-    r.headers["content-type"];      // application/json; charset=utf-8
-    r.text;                         // JSON text string
+  auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+		    cpr::Authentication{"user", "pass"},
+		    cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+  std::cout << r.status_code << std::endl;                  // 200
+  std::cout << r.header["content-type"] << std::endl;      // application/json; charset=utf-8
+  std::cout << r.text << std::endl;                         // JSON text string
 }
+
