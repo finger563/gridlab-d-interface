e3f8c029a1cf3c28d9999f1a8cb577ccaf1e2df3
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-12-10 20:05:06 -0800

    figuring out CPR

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-		    cpr::Authentication{"user", "pass"},
-		    cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-  std::cout << r.status_code << std::endl;                  // 200
-  std::cout << r.header["content-type"] << std::endl;      // application/json; charset=utf-8
-  std::cout << r.text << std::endl;                         // JSON text string
+  std::string _url = "http://www.httpbin.org/get";
+  if (argc > 1)
+    _url = argv[1];
+  std::cout << "Connecting to: " << _url << std::endl;
+  auto r = cpr::Get(cpr::Url{_url});
+  if (r.status_code >= 400) {
+    std::cerr << "Error [" << r.status_code << "] making request" << std::endl;
+  } else {
+    std::cout << "Request took " << r.elapsed << std::endl;
+    std::cout << "Header:" << std::endl << r.header["content-type"] << std::endl;
+    std::cout << "Body:" << std::endl << r.text << std::endl;
+  }
 }
 
