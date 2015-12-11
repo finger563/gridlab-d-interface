da0912ebd879a4656b1cf7505efb040db30680f3
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-12-10 21:16:11 -0800

    Updated test code and included gridlabd model for completness.

--- src/test.sh
+++ src/test.sh
@@ -2,17 +2,27 @@
 
 GLD=http://localhost:6267
 
+gridlabd -D run_realtime=1 Market_Controller_House_example.glm --server &
+
+PID=$!
+
+sleep 1
+
 # Show control interface testing
 ./gridlabd_interface ${GLD}/control/pauseat=2015-12-10
-sleep 10
+sleep 5
 ./gridlabd_interface ${GLD}/control/resume
-sleep 10
+sleep 5
 ./gridlabd_interface ${GLD}/control/pauseat=2015-12-10
-sleep 10
+sleep 5
 ./gridlabd_interface ${GLD}/control/resume
 
 # Show global variable GET
 ./gridlabd_interface ${GLD}/execdir
+sleep 1
+./gridlabd_interface ${GLD}/starttime
+sleep 1
+./gridlabd_interface ${GLD}/stoptime
 sleep 1
 
 # Show object property GET and SET
@@ -23,3 +33,5 @@ sleep 1
 ./gridlabd_interface ${GLD}/Market_1/current_price_mean_30min=40
 sleep 1
 ./gridlabd_interface ${GLD}/Market_1/current_price_mean_30min
+
+killall gridlabd.bin
