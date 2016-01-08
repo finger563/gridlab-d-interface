2302e1ef6450fd2aed5be9fbf0d53b81d83d3bc3
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-01-07 18:18:00 -0800

    Updated test script to better test altering loads in the model in a more realistic model.  Added some comments to hte model talking about market modeling.

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-model=$1
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-GLD=http://localhost:6267
+# Initialize our own variables:
+model="IEEE_13_Node_With_Houses.glm"
+GLD="http://localhost:6267"
+object="waterheater15"
+property="water_demand"
+unit="gpm"
+short_sleep=2
+long_sleep=5
+
+show_help() {
+    echo "Usage: ./test.sh -m <model file> -o <object name> -p <property name> -u <units>"
+    echo "       ./test.sh -m IEEE_13_Node_With_Houses.glm -o house15 -p heating_setpoint -u degF"
+}
+
+while getopts "h?m:o:p:u:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    m)  model=$OPTARG
+        ;;
+    o)  object=$OPTARG
+        ;;
+    p)  property=$OPTARG
+        ;;
+    u)  unit=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
 
 # start GLD child process in real-time server mode
 #gridlabd -D run_realtime=1 Market_Controller_House_example.glm --server &
@@ -11,34 +45,34 @@ gridlabd -D run_realtime=1 $model --server &
 PID=$!
 
 # wait for GLD to start up
-sleep 1
+sleep ${short_sleep}
 
 # Show control interface testing
 ./gridlabd_interface ${GLD}/control/pauseat=2015-12-10 12:32:59 CST
-sleep 5
+sleep ${long_sleep}
 ./gridlabd_interface ${GLD}/control/resume
-sleep 5
+sleep ${long_sleep}
 ./gridlabd_interface ${GLD}/control/pauseat=2015-12-10 15:16:17 CST
-sleep 5
+sleep ${long_sleep}
 ./gridlabd_interface ${GLD}/control/resume
 
 # Show global variable GET
 ./gridlabd_interface ${GLD}/execdir
-sleep 2
+sleep ${short_sleep}
 ./gridlabd_interface ${GLD}/starttime
-sleep 2
+sleep ${short_sleep}
 ./gridlabd_interface ${GLD}/stoptime
-sleep 2
+sleep ${short_sleep}
 
 # Show object property GET and SET
-./gridlabd_interface ${GLD}/Market_1/current_price_mean_30min=50
-sleep 2
-./gridlabd_interface ${GLD}/Market_1/current_price_mean_30min
-sleep 2
-./gridlabd_interface ${GLD}/Market_1/current_price_mean_30min=40
-sleep 2
-./gridlabd_interface ${GLD}/Market_1/current_price_mean_30min
+./gridlabd_interface ${GLD}/${object}/${property}=50${unit}
+sleep ${short_sleep}
+./gridlabd_interface ${GLD}/${object}/${property}
+sleep ${short_sleep}
+./gridlabd_interface ${GLD}/${object}/${property}=40${unit}
+sleep ${short_sleep}
+./gridlabd_interface ${GLD}/${object}/${property}
 
-sleep 5
+sleep ${short_sleep}
 
 killall gridlabd.bin
