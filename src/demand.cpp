7f5e22a29fe650ddedebd18f28dac96fc1997ed3
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-01-13 06:25:22 -0800

    refactoring and adding market scheduler program and demand scheduler program. need to add business logic to them.

---
+++ src/demand.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+  // parse cmd args
+
+  // set up the sockets for interfacing
+
+  // set up local vars
+  // set up the demand schedule structure here
+
+  // while loop for event handling
+  while (true)
+    {
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,17 @@
 #include "rapidxml.hpp"
 #include "rapidxml_utils.hpp"
 
-int main(int argc, char** argv) {
-  std::string _url = "http://localhost:6267";
-  if (argc > 1)
-    {
-      _url = "";
-      for (int i=1; i<argc; i++)
-	_url += argv[i] + std::string(" ");
-    }
-  std::cout << "Connecting to: " << _url << std::endl;
-  auto r = cpr::Get(cpr::Url{_url});
+
+bool gld_interface(std::string gld_url,
+		   std::string& object,  // in-out param
+		   std::string& name,    // in-out param
+		   std::string& value)   // in-out param
+{
+  std::cout << "Connecting to: " << gld_url << std::endl;
+  auto r = cpr::Get(cpr::Url{gld_url});
   if (r.status_code >= 400) {
     std::cerr << "Error [" << r.status_code << "] making request" << std::endl;
+    return false;
   } else {
     std::cout << "Request took " << r.elapsed << std::endl;
   }
@@ -28,28 +27,75 @@ int main(int argc, char** argv) {
       rapidxml::xml_document<> doc;
       doc.parse<0>((char *)r.text.c_str());
 
-      rapidxml::xml_node<> *type = doc.first_node("property");
-      rapidxml::xml_node<> *name, *val, *object;
-      if (type)
+      rapidxml::xml_node<> *_type = doc.first_node("property");
+      rapidxml::xml_node<> *_name, *_val, *_object;
+
+      object = "";
+      name = "";
+      value = "";
+
+      if (_type)
 	{
-	  object = type->first_node("object");
-	  name = type->first_node("name");
-	  val = type->first_node("value");
+	  _object = _type->first_node("object");
+	  _name = _type->first_node("name");
+	  _val = _type->first_node("value");
+	  object = _object->value();
+	  name = _name->value();
+	  value = _val->value();
+	  /*
 	  std::cout << "Received:" <<
 	    "\n\ttype = property" <<
-	    "\n\tobject = " << object->value() << 
-	    "\n\tname = " << name->value() << 
-	    "\n\tvalue = " << val->value() << std::endl;
+	    "\n\tobject = " << _object->value() << 
+	    "\n\tname = " << _name->value() << 
+	    "\n\tvalue = " << _val->value() << std::endl;
+	  */
 	}
       else
 	{
-	  type = doc.first_node("globalvar");
-	  name = type->first_node("name");
-	  val = type->first_node("value");
+	  _type = doc.first_node("globalvar");
+	  _name = _type->first_node("name");
+	  _val = _type->first_node("value");
+	  name = _name->value();
+	  value = _val->value();
+	  /*
 	  std::cout << "Received:" <<
 	    "\n\ttype = globalvar" <<
-	    "\n\tname = " << name->value() << 
-	    "\n\tvalue = " << val->value() << std::endl;
+	    "\n\tname = " << _name->value() << 
+	    "\n\tvalue = " << _val->value() << std::endl;
+	  */
 	}
     }
+  return true;
+}
+
+int main(int argc, char** argv) {
+
+  // parse cmd args
+  std::string gld_url = "http://localhost:6267";
+  if (argc > 1)
+    {
+      gld_url = "";
+      for (int i=1; i<argc; i++)
+	gld_url += argv[i] + std::string(" ");
+    }
+
+  // set up the sockets for interfacing
+
+  // set up local vars
+  int intf_retval = 0;
+  std::string object, name, value;
+
+  // while loop for event handling
+  while (true)
+    {
+      intf_retval = gld_interface(gld_url, object, name, value);
+      if (intf_retval) // everything went well
+	{
+	  std::cout << "Received:" <<
+	    "\n\tobject = " << object << 
+	    "\n\tname = " << name << 
+	    "\n\tvalue = " << value << std::endl;
+	}
+    }
+
 }
